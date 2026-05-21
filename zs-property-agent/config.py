a4fbe3b7745a98aa6984bb749701ea7a076940c5
Jacky <jacky@example.com>
2026-05-21 18:52:42 +0800

    feat: expand to 14 douyin + 2 xiaohongshu + 1 bilibili bloggers

--- zs-property-agent/config.py
+++ zs-property-agent/config.py
@@ -23,10 +23,31 @@ YOUTUBE_SEARCH_QUERIES = [
 ]
 
 DOUYIN_BLOGGERS = [
+    # Existing
     {"name": "罗宾Sir", "query": "罗宾Sir 中山 房产"},
     {"name": "中山小强总", "query": "中山小强总 房产"},
     {"name": "安个家中山东哥找房", "query": "东哥找房 中山"},
     {"name": "中山房探冉Sir", "query": "中山房探冉Sir"},
+    # New
+    {"name": "阿阳教买房", "query": "阿阳教买房 中山 房产"},
+    {"name": "胡须哥说房", "query": "胡须哥说房 中山 三乡"},
+    {"name": "中山亚文说房", "query": "中山亚文说房"},
+    {"name": "中山房产李先生jason", "query": "中山房产李先生jason"},
+    {"name": "双双看房", "query": "双双看房 中山"},
+    {"name": "中山一姐房产", "query": "中山一姐房产"},
+    {"name": "山姆说房咨询", "query": "山姆说房咨询 中山"},
+    {"name": "中山八登找房", "query": "中山八登找房"},
+    {"name": "中山房产吴同学", "query": "中山房产吴同学"},
+    {"name": "点点看房vlog", "query": "点点看房vlog 中山"},
+]
+
+XIAOHONGSHU_BLOGGERS = [
+    {"name": "罗宾Sir", "query": "罗宾Sir 中山 房产"},
+    {"name": "壹方置业", "query": "壹方置业 中山 房产"},
+]
+
+BILIBILI_BLOGGERS = [
+    {"name": "中山文英说房", "query": "中山文英说房"},
 ]
 
 REQUEST_TIMEOUT = 15
